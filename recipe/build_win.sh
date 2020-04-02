23f7ca027e84322213a14eacfa9eeca77e8beb58
Uwe L. Korn <uwe.korn@quantco.com>
2020-04-02 09:41:38 +0200

    Add final hack collection

--- recipe/build_win.sh
+++ recipe/build_win.sh
@@ -9,6 +9,7 @@ export BUILD_PREFIX_SH=/${BUILD_PREFIX_SH//:}
 sed -i -e "s;g++;${BUILD_PREFIX_SH}/Library/bin/clang++.exe;g" $BUILD_PREFIX/Lib/R/etc/x64/Makeconf
 sed -i -e "s;nm;${BUILD_PREFIX_SH}/Library/bin/llvm-nm.exe;g" $BUILD_PREFIX/Lib/R/etc/x64/Makeconf
 sed -i -e "s;gnu++11;gnu++14;g" $BUILD_PREFIX/Lib/R/etc/x64/Makeconf
+sed -i -e 's/attribute_visible/__declspec(dllexport)/g' $BUILD_PREFIX/Lib/R/library/Rcpp/include/RcppCommon.h
 
 # Patch Rcpp
 sed -i -e 's;abs;std::abs;g' $BUILD_PREFIX/Lib/R/library/Rcpp/include/Rcpp/DataFrame.h
@@ -16,4 +17,5 @@ sed -i -e 's;abs;std::abs;g' $BUILD_PREFIX/Lib/R/library/Rcpp/include/Rcpp/DataF
 # Patch build rules
 sed -i -e 's/@ tmp\.def/@/g' $BUILD_PREFIX/Lib/R/share/make/winshlib.mk
 
+sed -i -e 's/R_init_arrow/R_init_lib_arrow/g' r/src/arrowExports.cpp
 sed -i -e 's/useDynLib(arrow/useDynLib(lib_arrow/g' r/NAMESPACE
