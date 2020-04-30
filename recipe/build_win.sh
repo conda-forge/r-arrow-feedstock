0288b043dd465b73ed2667cfab1cf68180c14065
Uwe L. Korn <uwe.korn@quantco.com>
2020-04-30 15:56:44 +0200

    Patch RCpp

--- recipe/build_win.sh
+++ recipe/build_win.sh
@@ -9,3 +9,6 @@ export BUILD_PREFIX_SH=/${BUILD_PREFIX_SH//:}
 sed -i -e "s;g++;${BUILD_PREFIX_SH}/Library/bin/clang++.exe;g" $BUILD_PREFIX/Lib/R/etc/x64/Makeconf
 sed -i -e "s;nm;${BUILD_PREFIX_SH}/Library/bin/llvm-nm.exe;g" $BUILD_PREFIX/Lib/R/etc/x64/Makeconf
 sed -i -e "s;gnu++11;gnu++14;g" $BUILD_PREFIX/Lib/R/etc/x64/Makeconf
+
+# Patch Rcpp
+sed -i -e 's;abs;std::abs;g' $BUILD_PREFIX/Lib/R/library/Rcpp/include/Rcpp/DataFrame.h
