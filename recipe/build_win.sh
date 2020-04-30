e1d3ed7ad955b15068521157b9e0efd10cf7aaaf
Uwe L. Korn <uwe.korn@quantco.com>
2020-04-30 19:18:47 +0200

    Remove tmp.def from build rules

--- recipe/build_win.sh
+++ recipe/build_win.sh
@@ -12,3 +12,6 @@ sed -i -e "s;gnu++11;gnu++14;g" $BUILD_PREFIX/Lib/R/etc/x64/Makeconf
 
 # Patch Rcpp
 sed -i -e 's;abs;std::abs;g' $BUILD_PREFIX/Lib/R/library/Rcpp/include/Rcpp/DataFrame.h
+
+# Patch build rules
+sed -i -e 's/@ tmp\.def/@/g' $BUILD_PREFIX/Lib/R/share/make/winshlib.mk
