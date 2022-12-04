8c3167ee729769b328278919863ede8fb2531b62
H. Vetinari <h.vetinari@gmx.com>
2022-12-04 21:08:01 +1100

    sync with upstream; be explicit about -std=c++17

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -ex
 
 export DISABLE_AUTOBREW=1
 
-# shellcheck disable=SC2086
-${R} CMD INSTALL --build r/. ${R_ARGS}
+# set C++17 due to abseil
+export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -std=c++17"
+
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+$R CMD INSTALL --build r/.
