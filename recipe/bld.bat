5d9ca602a3bbc71e4f897b1e403241f053681adb
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-29 16:22:48 +0100

    Use win-native commands

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,10 +3,10 @@
 bash %RECIPE_DIR%/build_win.sh
 IF %ERRORLEVEL% NEQ 0 exit 1
 
-cp %RECIPE_DIR%/configure.win r
+copy %RECIPE_DIR%/configure.win r
 IF %ERRORLEVEL% NEQ 0 exit 1
 
-cp %RECIPE_DIR%/install.libs.R r/src
+copy %RECIPE_DIR%/install.libs.R r/src
 IF %ERRORLEVEL% NEQ 0 exit 1
 
 set "MAKEFLAGS=-j%CPU_COUNT%"
