ce6ed2bfa30eecb93c7dbeaf8a704e647ef82dbe
Uwe L. Korn <xhochy@users.noreply.github.com>
2024-02-29 20:52:04 +0100

    bld.bat aktualisieren

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ IF %ERRORLEVEL% NEQ 0 exit 1
 copy %RECIPE_DIR%\configure.win r
 IF %ERRORLEVEL% NEQ 0 exit 1
 
-copy %RECIPE_DIR%/install.libs.R r/src
+copy %RECIPE_DIR%\install.libs.R r\src
 IF %ERRORLEVEL% NEQ 0 exit 1
 
 set "MAKEFLAGS=-j%CPU_COUNT%"
