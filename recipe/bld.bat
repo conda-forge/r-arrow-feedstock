66795b61c53b0b88aced10a48225d5ebe5d764f3
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-01 17:22:19 +0100

    Print MAKEVARS

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,8 @@ IF %ERRORLEVEL% NEQ 0 exit 1
 
 echo "CXX17STD=-std=c++17" >>%R_MAKEVARS_SITE%
 
+type %R_MAKEVARS_SITE%
+
 set "MAKEFLAGS=-j%CPU_COUNT%"
 "%R%" CMD INSTALL --build r
 IF %ERRORLEVEL% NEQ 0 exit 1
