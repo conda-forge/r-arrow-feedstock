01ef4e72a2528e7db95f3ca2d97ddf0a8725db8b
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-01 17:02:09 +0100

    Add to makevars

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,6 +9,8 @@ IF %ERRORLEVEL% NEQ 0 exit 1
 copy %RECIPE_DIR%\install.libs.R r\src
 IF %ERRORLEVEL% NEQ 0 exit 1
 
+echo "CXX17STD=-std=c++17" >>%R_MAKEVARS_SITE%
+
 set "MAKEFLAGS=-j%CPU_COUNT%"
 "%R%" CMD INSTALL --build r
 IF %ERRORLEVEL% NEQ 0 exit 1
