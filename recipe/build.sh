6eb4ffeec02dd3e27755e5f693a586f9002fe137
Uwe Korn <uwelk@xhochy.com>
2026-03-13 16:17:31 +0000

    Add CXX20 on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,18 @@ set -ex
 
 export DISABLE_AUTOBREW=1
 
+# Add CXX20 if not set by toolchain
+if [[ -z "$("${R}" CMD config CXX20 2>/dev/null)" ]]; then
+    r_makevars_user="$(mktemp)"
+    cat > "${r_makevars_user}" <<EOF
+CXX20=${CXX}
+CXX20STD=-std=gnu++20
+CXX20FLAGS=${CXXFLAGS}
+CXX20PICFLAGS=-fpic
+EOF
+    export R_MAKEVARS_USER="${r_makevars_user}"
+fi
+
 # arrow uses C++20
 export ARROW_R_CXXFLAGS="${ARROW_R_CXXFLAGS} -std=c++20"
 export LIBARROW_BUILD=false
